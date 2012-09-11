e4d382d10ef08c47b2ec5468b6963e5edeb4b10b
Dmitry Agafonov <AgafonovDmitry@gmail.com>
2012-09-11 11:54:07 +0400

    its crap, start refactoring

--- siteslidr.js
+++ siteslidr.js
@@ -13,20 +13,31 @@
 
     var VERSION = [0, 0, 1],
 
-        stage = {
-            'prepared': false,
-            'title': null,
-            'overlay': null,
-            'sites': [],
-            'background': null
+        // Default settings for an app
+        defaults = {
+            'background': '#333'
         },
 
+        // Default site settings
+        // (site is a synonym for slide here and below)
         site_defaults = {
             'url': 'siteslidr_0.html',
             'duration': 5,
             'transition': 'fade'
         },
 
+        // Runtime data
+        stage = {
+            'root': null,
+            'title': null,
+            'overlay': null,
+            'sites': [],
+            'current': 0,
+            'background': null,
+            'timer': null
+        },
+
+        // XXX remove???
         fatal_error = function (e) {
             console.log(e);
             throw ('Error.');
@@ -36,21 +47,32 @@
         // Will be set by init()
         $ = null,
 
-        init = function () {
+        // Set all things up
+        init = function (config) {
             console.log('init()');
-            // Is everything is ready?
-            if (stage.prepared) {
+            // Is stage ready?
+            if (stage.root) {
                 return true;
             }
 
             // Check and install jQuery lib
-            // XXX TODO XXX
+            // XXX TODO install from cdn if not available here XXX
             $ = window.jQuery;
 
-            // Prepare stage
-            document.write("<iframe id='SiteA' src='site_a.html'></iframe>");
-            document.write("<iframe id='SiteB' src='site_b.html'></iframe>");
-            stage.prepared = true;
+            // Prepare the stage, if not defined by user
+            stage.root = $('#SiteSlidr_Stage');
+            if (stage.root.length === 0) {
+                stage.root = $('<div id="SiteSlidr_Stage"></div>').hide();
+                $('body').append(stage.root);
+            }
+
+            // Add all the slides to stage
+            for (var i = config.sites.length - 1; i >= 0; i--) {
+                var siteid = SiteSlidr.add(config.sites[i])
+                stage.sites.push({'site': $('#'+ siteid), 'id': siteid} );
+            }
+            stage.current = 0;
+            stage.root.fadeIn();
             return true;
         };
 
@@ -62,9 +84,15 @@
             return VERSION.join('.');
         },
 
+        // Create site frame and add to stage.
+        // Initial state is hidden and url XXX TODO not loaded XXX.
         'add': function (siteconfig) {
             console.log('add()');
-            return false;
+            var siteframe = $("<iframe id='" +
+                siteconfig.id + "' class='SiteSlidr_Frame' src='" +
+                siteconfig.url + "'></iframe>").hide();
+            stage.root.append(siteframe);
+            return siteconfig.id;
         },
 
         'delete': function (siteid) {
@@ -74,17 +102,29 @@
 
         'go': function (siteid) {
             console.log('go()');
-            return false;
+            stage.sites[stage.current].site.fadeOut(1000, function () {
+                stage.sites[1].site.fadeIn(1000);
+            });
         },
 
         'pause': function () {
             console.log('pause()');
-            return true;
+            if (stage.timeout) {
+                window.clearTimeout(stage.timeout);
+                stage.timeout = null;
+            }
+        },
+
+        'play': function () {
+            console.log('play()');
+            if (!stage.timeout) {
+                stage.timeout = window.setTimeout(SiteSlidr.next, 5000);
+            }
         },
 
         'rewind': function () {
             console.log('rewind()');
-            return true;
+            SiteSlidr.go(stage.sites[0])
         },
 
         'stop': function () {
@@ -93,7 +133,17 @@
 
         'next': function () {
             console.log('next()');
-            return false;
+            stage.sites[stage.current].site.fadeOut(1000, function () {
+                if (stage.current === stage.sites.length) {
+                    stage.current = 0;
+                } else {
+                    stage.current += 1;
+                }
+                console.log(stage.sites);
+                stage.sites[stage.current].site.fadeIn(1000);
+            });
+            window.clearTimeout(stage.timeout);
+            stage.timeout = window.setTimeout(SiteSlidr.next, 5000);
         },
 
         'previous': function () {
@@ -106,15 +156,18 @@
                 if (config.sites[0].url === '') {
                     throw ('NotConfigured');
                 } else {
-                    init();
-                    this.go(config.sites[0].id);
+                    init(config);
                 }
             } catch (e) {
-                fatal_error('SiteSlidr was not configured. Read the docs at http://github.com/agafonovdmitry/siteslidr');
+                fatal_error('SiteSlidr was not properly configured:' + e + ' ' +
+                'Read the docs at http://github.com/agafonovdmitry/siteslidr');
             }
-            $('#SiteB').fadeOut(1000, function () {
-                $('#SiteB').fadeIn(1000);
-            });
+            SiteSlidr.rewind();
+            SiteSlidr.play();
+            console.log(stage);
+//            stage.sites[1].fadeOut(1000, function () {
+//                stage.sites[1].fadeIn(1000);
+//            });
         }
 
     };
